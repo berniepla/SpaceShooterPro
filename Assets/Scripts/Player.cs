60b6bb24f9c6b14eda13eebf99df7ceb632ab5df
Bernie Pla <bpla@dolphins.com>
2022-11-29 17:09:20 -0500

    setting player bounds
    set on the y axis currently

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,7 +19,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // get user input
         float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // perform movement based on user input
+        //optimized a bit to use one Vector3 object
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // check if player position on the y is greater than 0
+        // y = 0
+        // else if position on y is less than -3.8f
+        // y = -3.8f
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        // if player on x > 11
+        // x  = -11
+        // else if player on x is less than -11
+        // x = 11
     }
 }
