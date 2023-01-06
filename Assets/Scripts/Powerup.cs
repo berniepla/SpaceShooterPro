83d660d5385d0e2579f398f5486812d0396b4725
Bernie Pla <bpla@dolphins.com>
2023-01-06 15:16:06 -0500

    starting the new powerups
    creating a modular version of the power up script to generate powerups based on ID

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -7,6 +7,13 @@ public class Powerup : MonoBehaviour
     [SerializeField]
     private float _speed = 3.0f;
 
+    //ID for Powerups
+    //0 is tripleshot
+    //1 is speed
+    //2 is shields
+    [SerializeField]
+    private int _powerupID;
+
     // Update is called once per frame
     private void Update()
     {
@@ -26,7 +33,21 @@ public class Powerup : MonoBehaviour
 
             if (player != null)
             {
-                player.TripleShotActive();
+                //if powerup id is 0
+                if (_powerupID == 0)
+                {
+                    player.TripleShotActive();
+                }
+                else if (_powerupID == 1)
+                {
+                    Debug.Log("got speed powerup");
+                }
+                else if (_powerupID == 2)
+                {
+                    Debug.Log("got shield powerup");
+                }
+                // else if powerup is 1, play speed powerup
+                //else if powerup is 2, shields powerup
             }
             Destroy(this.gameObject);
         }
